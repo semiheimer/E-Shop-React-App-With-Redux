86d1569760565c766eeddd74cc8dbc26928889b4
Semih Kutlu <skutlu@gmail.com>
2022-06-09 22:45:07 +0300

    responsive slide added

--- src/components/Products/Home/TopProducts.js
+++ src/components/Products/Home/TopProducts.js
@@ -38,6 +38,32 @@ function TopProducts({ products }) {
     slidesToScroll: 5,
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
